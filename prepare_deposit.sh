240e3bf3b34a5bda3c5e545ee7c685f91151c0d5
Lars Vilhuber <lars.vilhuber@cornell.edu>
2022-05-24 19:16:21 -0400

    Minor edits to script

--- prepare_deposit.sh
+++ prepare_deposit.sh
@@ -2,6 +2,12 @@
 
 outfile=deposit-repository-$(date +%F).zip
 
-zip -rp $outfile README.* tables/ programs/ images/
-zip -rp $outfile data/jira/anon  data/*txt data/icpsr data/manual data/scholarone
+zip -rp $outfile README.* 
+zip -rp $outfile tables/ 
+zip -rp $outfile programs/ 
+zip -rp $outfile images/*
+zip -rp $outfile data/jira/anon  
+zip -rp $outfile data/icpsr 
+zip -rp $outfile data/manual/ 
+zip -rp $outfile data/scholarone/*
 zip -rp $outfile $(find data -name README.\*)
